84f5cc706a590739ea81f4a8c7fa7ba9735ac4b2
jotave <joao.v.souza193@aluno.senai.br>
2026-03-20 10:45:22 -0300

    Add files via upload

---
+++ data/supabase.js
@@ -0,0 +1,12 @@
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    console.error('SUPABASE_URL ou SUPABASE_KEY nao foi encontrado no .env');
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
